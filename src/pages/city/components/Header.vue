183bb79049b92ddebde0915e2885ff4cc327af13
chenxiaoxi <chenxiaoxi@91isoft.com>
2018-08-03 17:27:55 +0800

    city-router

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '~style/varibles.scss';
+  .header{
+    height: $headerHeight;
+    line-height: $headerHeight;
+    overflow: hidden;
+    text-align: center;
+    color: #FFF;
+    background: $bgColor;
+    font-size: .32rem;
+    position: relative;
+    .header-back{
+      text-align: center;
+      font-size: .4rem;
+      width: .64rem;
+      position: absolute;
+      top: 0;
+      left: 0;
+      color: #FFF;
+    }
+  }
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -2,7 +2,9 @@
   <div class="header">
     <div class="header-left"><div class="iconfont back-icon">&#xe624;</div></div>
     <div class="header-input"><span class="iconfont">&#xe632;</span>输入城市/景点/游玩主题</div>
-    <div class="header-right">{{ city }} <span class="iconfont city-icon">&#xe64a;</span></div>
+    <router-link to="city">
+      <div class="header-right">{{ city }} <span class="iconfont city-icon">&#xe64a;</span></div>
+    </router-link>
   </div>
 </template>
 
@@ -18,7 +20,7 @@ export default {
 <style lang="scss" scoped>
   @import '~style/varibles.scss';
   .header{
-    line-height: .86rem;
+    line-height: $headerHeight;
     display: flex;
     background: $bgColor;
     color: #FFF;
@@ -45,6 +47,7 @@ export default {
       float: right;
       width: 1.24rem;
       text-align: center;
+      color: #FFF;
       .city-icon{
         margin-left: -0.04rem;
         font-size: .24rem;
