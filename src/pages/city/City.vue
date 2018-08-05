3232a27c6fe92914926e807b97329f8f66f6c29c
chenxiaoxi <chenxiaoxi@91isoft.com>
2018-08-05 09:43:24 +0800

    ajax-city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,8 @@
    <div>
       <city-header></city-header>
       <city-search></city-search>
-      <city-list></city-list>
-      <city-alphabet></city-alphabet>
+      <city-list :cities="cities" :hot="hotCities"></city-list>
+      <city-alphabet :cities="cities"></city-alphabet>
    </div>
 </template>
 
@@ -19,6 +19,29 @@ export default {
     CitySearch,
     CityList,
     CityAlphabet
+  },
+  data () {
+    return {
+      cities: {},
+      hotCities: []
+    }
+  },
+  methods: {
+    getCity () {
+      this.axios.get('api/city.json')
+        .then(this.getCitysSucc)
+    },
+    getCitysSucc (res) {
+      res = res.data
+      if( res.ret && res.data) {
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    }
+  },
+  mounted () {
+    this.getCity()
   }
 }
 </script>
